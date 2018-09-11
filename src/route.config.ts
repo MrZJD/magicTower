193c5be0b0d0962c29d30f6033ae219a8de686d0
mrzjd <390303304@qq.com>
2018-09-11 14:06:36 +0800

    add route

---
+++ src/route.config.ts
@@ -0,0 +1,11 @@
+import DemoScene from './scene/demo.scene';
+
+export default {
+    entry: 'welcome',
+    routes: [
+        {
+            name: 'welcome',
+            scene: DemoScene
+        }
+    ]
+}
